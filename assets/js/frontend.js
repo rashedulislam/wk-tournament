ef99234dc6a1f75a331fb7a8851caf448fee0c80
Md Rashedul Islam <rashedulislam.ruet@gmail.com>
2024-05-18 03:55:11 +0600

    added alert system, settings page and match post type

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,18 +1,61 @@
 jQuery(document).ready(function ($) {
+  function showSuccessMessage(message) {
+    Swal.fire({
+      icon: "success",
+      title: "Success",
+      text: message,
+    });
+  }
+
+  // Function to show error message
+  function showErrorMessage(message) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: message,
+    });
+  }
+
   $("#fetchTournaments").click(function () {
+    Swal.showLoading();
+    let tournamentID = $("#tournament-id").val();
     $.post({
       url: wkTournament.ajaxurl,
       data: {
         action: "wk_fetch_tournaments",
         _wpnonce: wkTournament.nonce,
+        tournamentID: tournamentID,
       },
       success: function (response) {
         console.log(response);
         if (!response.success) {
           console.log(response.data.message);
-          alert(response.data.message);
+          showErrorMessage(response.data.message);
         } else {
+          showSuccessMessage(response.data.message);
           console.log("Data:", response.data);
+        }
+      },
+    });
+  });
+
+  $("#fetchMatches").click(function () {
+    Swal.showLoading();
+    let tournamentID = $("#tournament-id").val();
+    $.post({
+      url: wkTournament.ajaxurl,
+      data: {
+        action: "wk_fetch_tournament_schedule",
+        _wpnonce: wkTournament.nonce,
+        tournamentID: tournamentID,
+      },
+      success: function (response) {
+        console.log(response);
+        if (!response.success) {
+          console.log(response.data.message);
+          showErrorMessage(response.data.message);
+        } else {
+          showSuccessMessage(response.data.message);
         }
       },
     });
--- wk-tournament.php
+++ wk-tournament.php
@@ -10,7 +10,6 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: wk-tournament
  * Domain Path: /languages
- * php version 8.1.2
 */
 
 require __DIR__.'/vendor/autoload.php';
@@ -21,7 +20,7 @@ $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 $api_key = $_ENV['API_KEY'];
 
-add_action('wp_enqueue_scripts', 'wk_tournament_form_assets_enqueue');
+
 function wk_tournament_form_assets_enqueue(){
     $file = plugin_dir_path(__FILE__) . '/assets/js/frontend.js';
     wp_enqueue_script('wk-tournament-frontend-script', plugins_url('/assets/js/frontend.js', __FILE__), array('jquery'), filemtime($file), true);
@@ -37,6 +36,32 @@ function wk_tournament_form_assets_enqueue(){
         ] 
     );
 }
+
+
+function enqueue_sweetalert2_admin() {
+    wp_enqueue_script('sweetalert2', 'https://cdn.jsdelivr.net/npm/sweetalert2@11', array('jquery'), '11', true);
+
+    wp_enqueue_style('sweetalert2-css', 'https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css', array(), '11');
+}
+
+
+function wk_tournament_admin_assets_enqueue() {
+	if (is_admin() && isset($_GET['page']) && $_GET['page'] === 'tournament-settings') {
+        wk_tournament_form_assets_enqueue();
+		enqueue_sweetalert2_admin();
+	}
+}
+add_action('admin_enqueue_scripts', 'wk_tournament_admin_assets_enqueue');
+
+
+function wk_tournament_frontend_assets_enqueue() {
+    wk_tournament_form_assets_enqueue();
+}
+add_action('wp_enqueue_scripts', 'wk_tournament_frontend_assets_enqueue');
+
+
+
+
 
 add_action( 'init', 'wk_tournament_register_post_type' );
 function wk_tournament_register_post_type(){
@@ -62,7 +87,7 @@ function wk_tournament_register_post_type(){
 	'public'             => true,
 	'publicly_queryable' => true,
 	'show_ui'            => true,
-	'show_in_menu'       => true,
+	'show_in_menu'       => false,
 	'query_var'          => true,
 	'rewrite'            => array( 'slug' => 'tournament' ),
 	'capability_type'    => 'post',
@@ -89,7 +114,7 @@ function custom_columns_head($defaults) {
 add_filter('manage_tournament_posts_columns', 'custom_columns_head');
 
 // Populate custom columns with custom field values
-function custom_columns_content($column_name, $post_ID) {
+function wk_tournament_posts_custom_columns_content($column_name, $post_ID) {
     switch ($column_name) {
         case 'seasonid':
             echo get_post_meta($post_ID, 'seasonid', true);
@@ -108,7 +133,7 @@ function custom_columns_content($column_name, $post_ID) {
             break;
     }
 }
-add_action('manage_tournament_posts_custom_column', 'custom_columns_content', 10, 2);
+add_action('manage_tournament_posts_custom_column', 'wk_tournament_posts_custom_columns_content', 10, 2);
 
 
 add_shortcode('get_tournaments_button', 'wk_get_tournaments_button_shortcode' );
@@ -132,12 +157,11 @@ function wk_fetch_tournaments_callback(){
 		);
 	}
 
-	$seasonID = 42;
+	$seasonID = sanitize_text_field($_POST['tournamentID']);
 
 	$api_url = 'http://api.sportsadmin.dk/api/v1/GetTournaments?seasonID=' . $seasonID;
 
 	$response = wp_remote_get($api_url, array('headers' => array('ApiKey' => $api_key)));
-	error_log(print_r( $response, 1 ));
 
 	if (!is_wp_error($response)) {
 		if ($response['response']['code'] === 200) {
@@ -207,3 +231,274 @@ function wk_fetch_tournaments_callback(){
 	}
 
 }
+
+
+/**
+ * Registers a custom post type for matches.
+ * 
+ * This function is responsible for creating a new custom post type called 'match'
+ * with the WordPress backend, allowing users to manage 'Match' content separately.
+ */
+function wk_tournament_register_post_type_matches() {
+	$labels = array(
+		'name'               => _x('Matches', 'post type general name', 'wk-tournament'),
+		'singular_name'      => _x('Match', 'post type singular name', 'wk-tournament'),
+		'menu_name'          => _x('Matches', 'admin menu', 'wk-tournament'),
+		'name_admin_bar'     => _x('Match', 'add new on admin bar', 'wk-tournament'),
+		'add_new'            => _x('Add New', 'match', 'wk-tournament'),
+		'add_new_item'       => __('Add New Match', 'wk-tournament'),
+		'new_item'           => __('New Match', 'wk-tournament'),
+		'edit_item'          => __('Edit Match', 'wk-tournament'),
+		'view_item'          => __('View Match', 'wk-tournament'),
+		'all_items'          => __('All Matches', 'wk-tournament'),
+		'search_items'       => __('Search Matches', 'wk-tournament'),
+		'parent_item_colon'  => __('Parent Matches:', 'wk-tournament'),
+		'not_found'          => __('No matches found.', 'wk-tournament'),
+		'not_found_in_trash' => __('No matches found in Trash.', 'wk-tournament')
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_in_rest'       => true,
+		'show_ui'            => true,
+		'show_in_menu'       => false,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'match' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 55,
+		'supports'           => array( 'title' )
+	);
+
+	register_post_type('match', $args);
+}
+add_action('init', 'wk_tournament_register_post_type_matches');
+
+
+/**
+ * Shortcode function to generate a button that fetches matches.
+ *
+ * This function returns an HTML button element with an ID that
+ * can be used to trigger an event, such as fetching matches from the database.
+ * 
+ * @return string HTML markup for the button.
+ */
+function wk_get_matches_button_shortcode(){
+	return '<button id="fetchMatches">'.__('Get Matches', 'wk-tournament').'</button>';
+}
+add_shortcode('get_matches_button', 'wk_get_matches_button_shortcode' );
+
+
+add_action('wp_ajax_wk_fetch_tournament_schedule', 'wk_fetch_tournament_schedule_callback' );
+add_action('wp_ajax_nopriv_wk_fetch_tournament_schedule', 'wk_fetch_tournament_schedule_callback');
+
+
+function wk_fetch_tournament_schedule_callback() {
+
+		global $api_key;
+	
+		if (! wp_verify_nonce($_REQUEST['_wpnonce'], 'get_wk_tournaments') ) {
+			wp_send_json_error(
+				[
+				'message' => __('Nonce verification failed!', 'wk-tournament')
+				 ] 
+			);
+		}
+
+		$tournamentID = sanitize_text_field($_POST['tournamentID']);
+	
+		$api_url = 'http://api.sportsadmin.dk/api/v1/GetTournamentSchedule?tournamentID=' . $tournamentID;
+	
+		$response = wp_remote_get($api_url, array('headers' => array('ApiKey' => $api_key)));
+	
+		if (!is_wp_error($response)) {
+			if ($response['response']['code'] === 200) {
+				$body = wp_remote_retrieve_body($response);
+	
+				$matches = json_decode($body);
+			
+				if (empty($matches)) {
+					wp_send_json_success(
+						[
+						'message' => __('No Matches found.!', 'wk-tournament')
+						]
+					);
+				}
+	
+				foreach ($matches as $match) {
+					$existingMatch = get_posts(array(
+						'post_type' => 'match',
+						'meta_key' => 'gameID',
+						'meta_value' => $match->gameID,
+						'posts_per_page' => 1
+					));
+
+
+					$officialsList = implode('<br>', array_map(function ($official) {
+						return esc_html($official->role) . ': ' . esc_html($official->name);
+					}, $match->gameOfficials));
+				
+					if ($existingMatch) {
+						$post_id = $existingMatch[0]->ID;
+						$updated_post_data = array(
+							'post_title' => $match->ArenaName,
+							'meta_input' => array(
+								'gameDate' => date('Y-m-d H:i:s', strtotime($match->gameDate)),
+								'ArenaName' => $match->ArenaName,
+								'oponents' => esc_html($match->homeTeamDisplayName) . ' vs. ' . esc_html($match->awayTeamDisplayName),
+								'score' =>  esc_html($match->homeTeamGoals) . ' - ' . esc_html($match->awayTeamGoals),
+								'homeTeamDisplayName' => $match->homeTeamDisplayName,
+								'awayTeamDisplayName' => $match->awayTeamDisplayName,
+								'homeTeamGoals' => $match->homeTeamGoals,
+								'awayTeamGoals' => $match->awayTeamGoals,
+								'officials' => $officialsList,
+								'gameID' => $match->gameID,
+								'leagueID' => $match->leagueID
+							)
+						);
+						wp_update_post(array_merge(['ID' => $post_id], $updated_post_data));
+					} else {
+						$new_post_data = array(
+							'post_title' => $match->ArenaName,
+							'post_type' => 'match',
+							'meta_input' => array(
+								'gameDate' => date('Y-m-d H:i:s', strtotime($match->gameDate)),
+								'ArenaName' => $match->ArenaName,
+								'oponents' => esc_html($match->homeTeamDisplayName) . ' vs. ' . esc_html($match->awayTeamDisplayName),
+								'score' =>  esc_html($match->homeTeamGoals) . ' - ' . esc_html($match->awayTeamGoals),
+								'homeTeamDisplayName' => $match->homeTeamDisplayName,
+								'awayTeamDisplayName' => $match->awayTeamDisplayName,
+								'homeTeamGoals' => $match->homeTeamGoals,
+								'awayTeamGoals' => $match->awayTeamGoals,
+								'officials' => $officialsList,
+								'gameID' => $match->gameID,
+								'leagueID' => $match->leagueID
+							),
+							'post_status' => 'publish'
+						);
+						wp_insert_post($new_post_data);
+					}
+				}
+				wp_send_json_success(
+					[
+					'message' => __('Match posts has been updated successfully!', 'wk-tournament')
+					]
+				);
+			} else {
+				$response_message = wp_remote_retrieve_response_message($response);
+				wp_send_json_error([
+					'message' => $response_message
+				]);
+			}
+			
+		} else {
+			$error_message = $response->get_error_message();
+			error_log(print_r( $error_message, 1 ));
+			wp_send_json_error([
+				'message' => $error_message
+			]);
+		}
+	
+	}
+
+
+function wk_tournament_schedule_posts_custom_columns_head($defaults) {
+	unset($defaults['date']);
+    $defaults['gameDate'] = 'Dating';
+    $defaults['ArenaName'] = 'Arena Name';
+    $defaults['oponents'] = 'Match';
+    $defaults['score'] = 'Score';
+    $defaults['officials'] = 'Officials';
+    $defaults['gameID'] = 'Game ID';
+    $defaults['leagueID'] = 'Tournament ID';
+    return $defaults;
+}
+add_filter('manage_match_posts_columns', 'wk_tournament_schedule_posts_custom_columns_head');
+
+	function wk_tournament_schedule_posts_custom_columns_content($column_name, $post_ID) {
+		switch ($column_name) {
+			case 'gameDate':
+				echo get_post_meta($post_ID, 'gameDate', true);
+				break;
+			case 'ArenaName':
+				echo get_post_meta($post_ID, 'ArenaName', true);
+				break;
+			case 'oponents':
+				echo get_post_meta($post_ID, 'oponents', true);
+				break;
+			case 'score':
+				echo get_post_meta($post_ID, 'score', true);
+				break;
+			case 'officials':
+				echo get_post_meta($post_ID, 'officials', true);
+				break;
+			case 'gameID':
+				echo get_post_meta($post_ID, 'gameID', true);
+				break;
+			case 'leagueID':
+				echo get_post_meta($post_ID, 'leagueID', true);
+				break;
+		}
+	}
+	add_action('manage_match_posts_custom_column', 'wk_tournament_schedule_posts_custom_columns_content', 10, 2);
+
+	function my_custom_menu() {
+		add_menu_page(
+			'Tournament Settings',
+			'Tournaments',
+			'manage_options',
+			'tournament-settings',
+			'tournament_settings_page',
+			'dashicons-admin-generic',
+			20
+		);
+	
+		add_submenu_page(
+			'tournament-settings',
+			'Tournament Settings',
+			'Settings',
+			'manage_options',
+			'tournament-settings',
+			'tournament_settings_page'
+		);
+	
+		add_submenu_page(
+			'tournament-settings',
+			'Tournament List',
+			'Tournaments',
+			'manage_options',
+			'edit.php?post_type=tournament'
+		);
+	
+		add_submenu_page(
+			'tournament-settings',
+			'Match List',
+			'Matches',
+			'manage_options',
+			'edit.php?post_type=match'
+		);
+	}
+	add_action('admin_menu', 'my_custom_menu');
+
+	function tournament_settings_page() {
+		?>
+		<div class="wrap">
+			<h1>Tournament Settings</h1>
+			<form id="tournament-settings-form">
+				<table class="form-table">
+					<tr valign="top">
+						<th scope="row">Tournament/Season ID</th>
+						<td><input type="text" id="tournament-id" name="tournament_id" value="" /></td>
+					</tr>
+				</table>
+				<button type="button" id="fetchTournaments" class="button button-primary" style="margin-right: 20px;">Fetch Season</button>
+				<button type="button" id="fetchMatches" class="button button-secondary">Fetch Tournament Schedule</button>
+			</form>
+			<div id="response"></div>
+		</div>
+    <?php
+}
+	
